5f226af72ce8384747f3680dcfba297293ac3d26
tascintra <tiago.slv@hotmail.com>
2022-10-21 10:20:24 -0300

    imask installation

--- src/main.js
+++ src/main.js
@@ -1,25 +1,22 @@
-import './css/index.css';
+import "./css/index.css"
+import IMask from "imask"
 
-const ccBgColor01 = document.querySelector(
-  '.cc-bg svg > g g:nth-child(1) path'
-);
-const ccBgColor02 = document.querySelector(
-  '.cc-bg svg > g g:nth-child(2) path'
-);
-const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img');
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
-    visa: ['#436D99', '#2D57F2'],
-    mastercard: ['#C69347', '#EB001B'],
-    elo: ['#00A4E0', '#FFCB05'],
-    amex: ['#0077A6', '#005B80'],
-    default: ['black', 'gray'],
-  };
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#C69347", "#EB001B"],
+    elo: ["#00A4E0", "#FFCB05"],
+    amex: ["#0077A6", "#005B80"],
+    default: ["black", "gray"],
+  }
 
-  ccBgColor01.setAttribute('fill', colors[type][0]);
-  ccBgColor02.setAttribute('fill', colors[type][1]);
-  ccLogo.setAttribute('src', `cc-${type}.svg`);
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-globalThis.setCardType = setCardType;
+globalThis.setCardType = setCardType
